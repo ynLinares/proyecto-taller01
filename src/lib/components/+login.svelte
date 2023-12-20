daf822b549b1a81957d79abb5d87320f61d46279
yahel1 <yahelnissim9@gmail.com>
2023-12-20 14:27:54 -0400

    html a svelte

--- src/lib/components/+login.svelte
+++ src/lib/components/+login.svelte
@@ -1,52 +1,67 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  import "$lib/assets/Estilos.css";
-  const dispatch = createEventDispatcher();
 
-  let nombre = "";
-  let pin = "";
+	import { createEventDispatcher } from "svelte";
+	import '$lib/assets/Estilos.css';
+	import { goto } from "$app/navigation";
+	const dispatch = createEventDispatcher();
 
-  function datos() {
-    dispatch("submit", { nombre, pin });
-  }
+	let nombre = '';
+	let pin = '';
+
+	function handleSubmit() {
+		dispatch('submit', { nombre, pin });
+		
+	}
 </script>
 
-<style>
-  .centro {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .cuadro {
-    width: 50%;
-  }
-
-  .pin {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #000;
-    border-radius: 5px;
-  }
-
-  .botton {
-    width: 100%;
-    height: 40px;
-    border: 1px solid #000;
-    border-radius: 5px;
-    background-color: #ffffff;
-    color: #000;
-    font-size: 18px;
-    font-weight: bold;
-  }
-</style>
-
 <div class="centro">
-  <img src="/ImagenesProyecto/Kahoot_Logo.svg.png" alt="Imagen1" style="background-color: transparent; width: 252px;"/>
-  <div class="cuadro">
-    <input type="text" class="pin" placeholder="Nombre de Usuario" bind:value={nombre} required />
-    <input type="text" class="pin" placeholder="Pin" bind:value={pin} required />
-    <button class="botton" on:click={datos}>Ingresar</button>
-  </div>
+	<img
+		src="/ImagenesProyecto/Kahoot_Logo.svg.png"
+		alt="Imagen1"
+		style="background-color: transparent; width: 252px;"
+	/>
+	<div class="cuadro">
+		<form on:submit={handleSubmit}>
+			<input
+				type="text"
+				class="pin"
+				placeholder="Nombre de Usuario"
+				bind:value={nombre}
+				required
+			/>
+			<input type="text" class="pin" placeholder="Pin" bind:value={pin} required />
+			<button class="botton">Ingresar</button>
+		</form>
+	</div>
 </div>
+
+<style>
+	.centro {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.cuadro {
+		width: 50%;
+	}
+
+	.pin {
+		width: 100%;
+		padding: 10px;
+		border: 1px solid #000;
+		border-radius: 5px;
+	}
+
+	.botton {
+		width: 100%;
+		height: 40px;
+		border: 1px solid #000;
+		border-radius: 5px;
+		background-color: #ffffff;
+		color: #000;
+		font-size: 18px;
+		font-weight: bold;
+	}
+</style>
