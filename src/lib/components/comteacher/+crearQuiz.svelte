71cbb5e8ee552a220690218450b96ee3f6c62c72
Geraldine Rangel <86479893+grangel20@users.noreply.github.com>
2024-01-19 16:57:42 -0400

    Dead

--- src/lib/components/comteacher/+crearQuiz.svelte
+++ src/lib/components/comteacher/+crearQuiz.svelte
@@ -2,22 +2,21 @@
 	import Head from '../comambos/+head.svelte';
 	import Titulo from '../comambos/+titulo.svelte';
 	import RecuadroPregunta from '../comambos/+recuadroPregunta.svelte';
-	import ImageTema from '../comambos/+imageTema.svelte';
     import Button from '../comambos/+button.svelte';
 
     // agregar condicional si no se tiene pregunas guardadas
     export let titulopregunta = 'pregunta 1 ';
-	export let nombreQuiz = 'Quiz xyz';
 	export let respuestas = ['Respuesta 1', 'Respuesta 2', 'Respuesta 3', 'Respuesta 4'];
 
 </script>
 
-<Head titulo={nombreQuiz}></Head>
+<Head titulo="Ahorcado"></Head>
 
 <div style="display: flex;">
 	<div>
-		<Titulo tituloChiquito={titulopregunta} />
+<div class="cuadro centro ">
 
+</div>
 		<div style="display: flex; justify-content: space-between;">
 			<div>
 				<RecuadroPregunta respuesta={respuestas[0]} />
@@ -30,23 +29,5 @@
 		</div>
 	</div>
 
-	<div style="width: 25%; max-width: none;">
-		<h2>Temas</h2>
-		<div class="grid-container">
-            <ImageTema url="/ImagenesProyecto/2.png" />
-            <ImageTema url="/ImagenesProyecto/2.png" />
-            <ImageTema url="/ImagenesProyecto/2.png" />
-            <ImageTema url="/ImagenesProyecto/2.png" />
-		</div>
 
-		<h3>Limite de tiempo</h3>
-		<select class="botton" style="margin: 0px;">
-			<option value="1">30 segundos</option>
-			<option value="2">40 segundos</option>
-			<option value="3">60 segundos</option>
-		</select>
-
-        <Button nombre = "Agregar pregunta " url ="/teacher/crearPregunta" />
-        <Button nombre = "Crear" url ="/teacher" />
-	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
 	import Inicioo from '$lib/components/comambos/+inicioo.svelte';
+	import CrearQuiz from '$lib/components/comteacher/+crearQuiz.svelte';
 </script>
 
 <div>
-	<div
+	<!-- <div
 		style="	display: flex;
             justify-content: center;
 	        align-items: center;">
 		<Inicioo name="player" url="/player" comoyoquiera="mira que lindo" />
 		<Inicioo name="admin" url="/teacher" comoyoquiera="per mi ra que lindo " />
-	</div>
+	</div> -->
+    <CrearQuiz />
 </div>
